c87bfafb0708a9e21aff15f3c6759aad3ed0f0ae
sopadelletres01 <marcportero01@gmail.com>
2022-07-15 11:14:49 +0200

    sort done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const listContainer = document.querySelector("#dog-list");
 const segundaColumna = document.querySelector(".segunda-columna");
 const search = document.querySelector("#search");
+const sort = document.querySelector("#sort");
 const errorMessage = document.querySelector(".error");
 
 const domain = "https://dog.ceo/api";
@@ -70,6 +71,30 @@ const setupSearch = () => {
   });
 };
 
+const setupSort = () => {
+  //He decidido hacer el sort en orden alfabetico descendiente en vez de ascendente porque la api devuelve los resultados ordenados de primeras
+  //De esta forma se puede apreciar que el sort funciona
+  sort.addEventListener("click",()=>{
+    let elements = document.querySelectorAll("div.breed");
+    let elems = Array.from(elements).sort((a,b)=>{
+      let aValue = a.classList[0] 
+      let bValue = b.classList[0] 
+      //Orden descendiente
+      if (aValue < bValue) {
+        return 1;
+      }
+      if (aValue > bValue) {
+        return -1;
+      }
+      return 0;
+    })
+    listContainer.innerHTML = ""
+    elems.forEach(elem=>{
+      listContainer.append(elem)
+    })
+  })
+}
+
 const runApp = async () => {
   const breedData = await getData("breeds/list/all");
   const entries = Object.entries(breedData);
@@ -77,6 +102,7 @@ const runApp = async () => {
   const images = listContainer.querySelectorAll(".img-click");
   setupGallery(images);
   setupSearch();
+  setupSort()
 };
 
 runApp();
